20bd43f383252e494becc078cc8b52b69eea013e
Alexis Chochina <alexis@fcinq.com>
2025-04-03 11:46:12 +0200

    fix deploy vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// @ts-ignore
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  typescript: {
+    strict: false,
+    typeCheck: false,
+    shim: false
+  },
+  build: {
+    transpile: ['three']
+  },
+  nitro: {
+    preset: 'vercel'
+  }
 })
